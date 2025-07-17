f42520529a69d9c6323dc27d02968712150cf84a
Rahul <rahulkrishlalwani@gmail.com>
2025-07-17 19:23:11 +0530

    production server running

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ export default async function Home() {
     const user = await client.user.findFirst(); 
     return <div>
         <h1 >This is Production server</h1>
+        <h2>Production Server running</h2>
         <div>Name: {user?.username}</div>
         <div>Password: {user?.password}</div>
     </div>
