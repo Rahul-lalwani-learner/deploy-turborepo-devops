66ac0a68e2e2b5b90dbcc3f0dfaea5a365da9ed8
Rahul <rahulkrishlalwani@gmail.com>
2025-07-17 18:53:36 +0530

    Changed the production server

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import { client } from "@repo/db/client"
 export default async function Home() {
     const user = await client.user.findFirst(); 
     return <div>
+        <h1 >This is Production server</h1>
         <div>Name: {user?.username}</div>
         <div>Password: {user?.password}</div>
     </div>
